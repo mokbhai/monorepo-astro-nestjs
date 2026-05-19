ed3e8539fed77cf41d0d43a7f69f921f3b757752
Mokshit Jain <mokshitjain18@gmail.com>
2026-05-19 19:40:42 +0530

    Update workspace dependencies

--- packages/ui/tsup.config.ts
+++ packages/ui/tsup.config.ts
@@ -3,7 +3,11 @@ import { defineConfig } from 'tsup';
 export default defineConfig({
   entry: ['src/index.ts'],
   format: ['cjs', 'esm'],
-  dts: true,
+  dts: {
+    compilerOptions: {
+      ignoreDeprecations: '6.0',
+    },
+  },
   clean: true,
   sourcemap: true,
   external: ['react', 'react-dom'],
